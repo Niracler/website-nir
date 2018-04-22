dff6065342a3a7ceef0f2416fc4ce23da47dc189
niracler <1026037967@qq.com>
2018-04-22 10:44:21 +0800

    数据库方面,用上了mysql

--- website_py/settings.py
+++ website_py/settings.py
@@ -75,11 +75,14 @@ WSGI_APPLICATION = 'website_py.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'web_db',
+        'USER': 'team',
+        'PASSWORD': '12345678',
+        'HOST': 'niracler.com',
+        'PORT': '3306',
     }
 }
 
@@ -122,5 +125,5 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),]
-STATIC_ROOT = os.path.join(BASE_DIR,'static')
+# STATIC_ROOT = os.path.join(BASE_DIR,'static')
 
