1893ae4bd22190017bcd635747c9e7ea33f2aaa6
niracler <1026037967@qq.com>
2018-09-13 17:02:41 +0800

    Change UI restart project

--- blog/urls.py
+++ blog/urls.py
@@ -3,8 +3,6 @@ from . import views
 
 urlpatterns = [
     path("", views.home, name="home"),
-    path('<int:article_id>', views.article_detail, name='article_detail'),
+    path('<int:article_id>', views.detail, name='detail'),
     path('category/<article_category_pk>', views.articles_with_category, name='articles_with_category'),
-    path('article_list', views.article_list, name='article_list'),
-    path('aboutme', views.aboutme, name='aboutme')
 ]
--- blog/views.py
+++ blog/views.py
@@ -4,24 +4,18 @@ from .models import Article, ArticleType
 
 
 # 这个文件装的是视图，即是网页的内容
-
-def home(request):
-    context = {}  # 要有这个，不然加载不出base
-    return render(request, "blog/home.html", context)
-
-
-def article_detail(request, article_id):
+def detail(request, article_id):
     article = get_object_or_404(Article, pk=article_id)
     context = {}
     context['article_obj'] = article
-    return render(request, "blog/article_detail.html", context)
+    return render(request, "blog/detail.html", context)
 
 
-def article_list(request):
+def home(request):
     context = {}
     context['articles_count'] = Article.objects.all().count()
     context['articles'] = Article.objects.all()
-    return render(request, "blog/article_list.html", context)
+    return render(request, "blog/home.html", context)
 
 
 def articles_with_category(request, article_category_pk):
@@ -29,8 +23,4 @@ def articles_with_category(request, article_category_pk):
     type_name = get_object_or_404(ArticleType, type_name=article_category_pk)
     context['articles_count'] = Article.objects.filter(type_name=type_name).count()
     context['articles'] = Article.objects.filter(type_name=type_name)
-    return render(request, "blog/article_list.html", context)
-
-
-def aboutme(request):
-    return render(request, "blog/aboutme.html")
+    return render(request, "blog/home.html", context)
