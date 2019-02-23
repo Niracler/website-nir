52167b074bb2f0c74d66de7c7d7d375f7be4ebbb
Niracler <1026037967@qq.com>
2019-02-23 19:05:28 +0800

    model的serializer

--- apps/goods/serializers.py
+++ apps/goods/serializers.py
@@ -1,7 +1,16 @@
 from rest_framework import serializers
+from goods.models import Goods, GoodsCategory
 
 
-class GoodsSerializer(serializers.Serializer):
-    name = serializers.CharField(required=True, max_length=100)
-    click_num = serializers.IntegerField(default=0)
-    category = serializers.ImageField()
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = GoodsCategory
+        fields = '__all__'
+
+
+class GoodsSerializer(serializers.ModelSerializer):
+    category = CategorySerializer()
+
+    class Meta:
+        model = Goods
+        fields = '__all__'
