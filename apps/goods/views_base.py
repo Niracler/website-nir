59fa66a35bb778f84f3a7f79c3c7732ac783080c
Niracler <1026037967@qq.com>
2019-02-23 10:16:24 +0800

    django的serializer序列化model

--- apps/goods/views_base.py
+++ apps/goods/views_base.py
@@ -14,13 +14,11 @@ class GoodsListView(View):
         json_list = []
         goods = Goods.objects.all()[:10]
 
-        for good in goods:
-            json_dict = {}
-            json_dict['name'] = good.name
-            json_dict['category'] = good.category.name
-            json_dict['market_price'] = good.market_price
-            json_list.append(json_dict)
-
+        from django.core import serializers
         from django.http import HttpResponse
         import json
-        return HttpResponse(json.dumps(json_list), content_type="application/json ")
+
+        json_data = serializers.serialize("json", goods)
+        json_data = json.loads(json_data)
+
+        return HttpResponse(json.dumps(json_data), content_type="application/json ")
