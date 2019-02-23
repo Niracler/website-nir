0ad0917ad25522530201efe1b31e1e126c2c5db4
Niracler <1026037967@qq.com>
2019-02-23 19:45:09 +0800

    用pagination的方法分页

--- apps/goods/views.py
+++ apps/goods/views.py
@@ -2,14 +2,22 @@ from django.http import JsonResponse
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import generics
+from rest_framework.pagination import PageNumberPagination
 from .models import Goods
 from .serializers import GoodsSerializer
+
+
+class GoodsPagination(PageNumberPagination):
+    page_size = 10
+    page_size_query_param = 'page_size'
+    page_query_param = 'p'
+    max_page_size = 100
 
 
 class GoodsListView(generics.ListAPIView):
     """
     商品列表页
     """
-
     queryset = Goods.objects.all()
     serializer_class = GoodsSerializer
+    pagination_class = GoodsPagination
